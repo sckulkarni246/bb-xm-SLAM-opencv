5bfc477876b32f9eebc296361ab7979e1f64d359
Shashank Kulkarni <shanky.kulkarni@gmail.com>
2012-05-16 14:51:11 +0530

    Commented the shell file and uploaded. This shell file may be avoided and a new one can be made to suit your purpose.

---
+++ runthis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This shell file can be changed as much as you want. It depends only on the implementation. Please use the shell file only after you are done with testing the 
+# executable.
+for ((c=1;c<=10;++c)) # change the terminating condition to change the number of times the executable runs - each increment gives 10 more runs
+do
+./capture 6 4         # run the executable - the first parameter is the number of peripheral circles, second is the number appended to logfile. eg: logfile4.txt
+echo $c               # display the value of c at the end of each cycle
+done
