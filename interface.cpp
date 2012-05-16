ac94c0572c07abc80c782adc52e8968201ca63ec
Shashank Kulkarni <shanky.kulkarni@gmail.com>
2012-05-16 16:11:06 +0530

    Commented the code and uploaded.

---
+++ interface.cpp
@@ -0,0 +1,33 @@
+/*
+This is a simple code that will allow you to test your camera and interface it using OpenCV. On execution, a window will open that will display the capture stream of the camera. It will end after ~100 seconds.
+*/
+#include "header.h"
+
+int main(void)
+{
+	IplImage *frame = NULL;
+	CvCapture* capture = cvCaptureFromCAM(CV_CAP_ANY);
+	if(!capture)
+		{
+		printf("No capture");
+		return 1;
+		}
+	cvNamedWindow("display",CV_WINDOW_AUTOSIZE);
+	int i=1;
+	while(i<10000)
+		{
+		frame = cvQueryFrame( capture );
+		if(!frame)
+			{
+			printf("Capture but null frame!");
+			return 2;
+			}
+		cvShowImage("display",frame);
+		cvWaitKey(10);
+		++i;
+		}
+	cvReleaseCapture( &capture );
+	cvReleaseImage( &frame );
+	cvDestroyWindow("display");
+	return 0;
+}
