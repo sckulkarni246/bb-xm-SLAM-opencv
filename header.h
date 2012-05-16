598e3a5b9b79cfe460b3f3c68f0d91fcc92009aa
Shashank Kulkarni <shanky.kulkarni@gmail.com>
2012-05-16 14:14:51 +0530

    Categorized the different headers and uploaded file.

---
+++ header.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_H_
+#define HEADER_H_
+#endif
+
+// Standard Includes
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>				// In case, you also want to log data
+
+// OpenCV Includes
+
+#include <cv.h>
+#include <highgui.h>			// If using Eclipse, try <opencv/cv.h>, <opencv/highgui.h> etc.
+
+// Other Includes
+
+#include <math.h>
+#include <string.h>
+#include <stddef.h>
+
