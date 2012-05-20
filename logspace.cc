a4eda7d354443ab3d616aa4962548a7c2daab365
Shashank Kulkarni <shanky.kulkarni@gmail.com>
2012-05-20 10:05:51 +0530

    Upped with header and credits comment.

--- logspace.cc
+++ logspace.cc
@@ -1,3 +1,7 @@
+/* Original code by: Shashank Kulkarni and Aditya Buddhavarapu. Please share and re-share this code. And give due credits
+wherever possible. Thank you! You are awesome!
+*/
+
 using namespace std;
 
 int logspace(char *filename)
