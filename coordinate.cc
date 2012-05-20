80d8124bd15d9055e5c8e4fd0e823ba564a6182e
Shashank Kulkarni <shanky.kulkarni@gmail.com>
2012-05-20 10:00:33 +0530

    Upped with header and credits comment.

--- coordinate.cc
+++ coordinate.cc
@@ -1,3 +1,7 @@
+/* Original code by: Shashank Kulkarni and Aditya Buddhavarapu. Please share and re-share this code. And give due credits
+wherever possible. Thank you! You are awesome!
+*/
+
 #include  "header.h"
 #include "distance.cc"
 
